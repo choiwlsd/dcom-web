a7f2664f21784f06c76b97ae3107ea3035f1cb8b
Jinyeong Choi <choiwlsd@gmail.com>
2026-05-30 17:16:33 +0900

    Complete: Footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,9 +4,9 @@ import { SiNotion } from "react-icons/si";
 
 export default function Footer() {
   return (
-    <footer className="flex h-16 mt-5 pl-3 pr-3 items-center justify-between bg-white/95 text-sm text-gray-400 backdrop-blur-sm">
+    <footer className="flex h-16 mt-5 pl-8 pr-9 items-center justify-between bg-white/95 text-sm text-gray-400 backdrop-blur-sm">
         <div className="flex items-center gap-7">
-            <img src={DCOMLogo} alt="DCOM Logo" className="h-16 object-contain pl-4 p-1 opacity-25" />
+            <img src={DCOMLogo} alt="DCOM Logo" className="h-16 object-contain p-1 opacity-25" />
             <p>since 1999</p>
             <p>
                 &copy; {new Date().getFullYear()} DCOM. All rights reserved.
@@ -14,12 +14,12 @@ export default function Footer() {
         </div>
         <div className="flex gap-5">
             <FaInstagram 
-                size={21} 
+                size={18} 
                 onClick={() => window.open("https://www.instagram.com/d.com_official", "_blank")} 
                 className="cursor-pointer"
             />
             <SiNotion 
-                size={21} 
+                size={18} 
                 onClick={() => window.open("https://www.notion.so", "_blank")} 
                 className="cursor-pointer" 
             />
