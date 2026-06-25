b7940ee0c3b6086f1f9a3667afcd07286543f249
Jinyeong Choi <choiwlsd@gmail.com>
2026-06-25 17:12:57 +0900

    Feat: section 구조 추가

--- src/pages/Notice.tsx
+++ src/pages/Notice.tsx
@@ -1,11 +1,15 @@
-import Container from "../components/ui/Container";
 
 const Notice = () => {
   return (
     <div className="px-4 py-8 sm:px-6 lg:px-20">
-      <Container title="공지사항">
-        <p>공지사항 내용</p>
-      </Container>
+      <section className="mb-10">
+        <h1 className="text-xl font-bold text-[#4988C4]">공지사항</h1>
+        <p className="mt-2 text-sm text-gray-500">
+          D.COM 내의 공지사항을 확인해보세요
+        </p>
+      </section>
+
+      
    </div>
   );
 };
